7767b73ea3161079ea29f403b9cf0372370d4b43
S-Mukherjee98 <shubhramukherjee4@gmail.com>
2025-06-22 10:42:59 +0530

    Change password attached

--- adhar/users/serializers.py
+++ adhar/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -17,8 +18,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
 
 
-
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id','name', 'email', 'age']
+        
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True, validators=[validate_password])
